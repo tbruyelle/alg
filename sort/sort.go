c16beb49627dd88fdd255ff8b0fefd41c968c6a0
Thomas Bruyelle <thomas.bruyelle@gmail.com>
2014-09-25 17:43:17 +0200

    Merge sort

--- sort/sort.go
+++ sort/sort.go
@@ -2,6 +2,55 @@ package sort
 
 import "fmt"
 
+// MergeSort sorts the array with the merge sort algorithm.
+func MergeSort(a []int) {
+	aux := make([]int, len(a))
+	mergeSort(a, aux, 0, len(a)-1)
+}
+
+func BottomUpMergeSort(a []int) {
+	//N := len(a)
+	//aux := make([]int, N)
+
+}
+
+func mergeSort(a, aux []int, lo, hi int) {
+	if lo >= hi {
+		return
+	}
+	mid := lo + (hi-lo)/2
+	mergeSort(a, aux, lo, mid)
+	mergeSort(a, aux, mid+1, hi)
+	merge(a, aux, lo, mid, hi)
+	fmt.Println("merging", lo, mid, hi, a)
+}
+
+func merge(a, aux []int, lo, mid, hi int) {
+	// Copy
+	for k := lo; k <= hi; k++ {
+		aux[k] = a[k]
+	}
+	// Merge
+	i, j := lo, mid+1
+	for k := lo; k <= hi; k++ {
+		switch {
+		case i > mid:
+			a[k] = aux[j]
+			j++
+		case j > hi:
+			a[k] = aux[i]
+			i++
+		case aux[j] < aux[i]:
+			a[k] = aux[j]
+			j++
+		default:
+			a[k] = aux[i]
+			i++
+		}
+	}
+}
+
+// HSort h-sorts the array given the h parameter.
 func HSort(h int, a []int) {
 	for i := h; i < len(a); i++ {
 		for j := i; j >= h; j-- {
--- sort/sort_test.go
+++ sort/sort_test.go
@@ -5,7 +5,18 @@ import (
 	"testing"
 )
 
+func TestMergeSort(t *testing.T) {
+	fmt.Println("MergeSort")
+	a := []int{29, 74, 47, 13, 61, 21, 56, 80, 42, 88, 67, 84}
+	fmt.Println(a)
+
+	MergeSort(a)
+
+	fmt.Println(a)
+}
+
 func TestHSort(t *testing.T) {
+	fmt.Println("HSort")
 	a := []int{43, 71, 67, 57, 29, 42, 53, 44, 24, 78}
 	fmt.Println(a)
 
