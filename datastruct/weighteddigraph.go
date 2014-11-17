1753c5b97c53f0e0f0c0c6849e87cb8c96cbfd91
Thomas Bruyelle <thomas.bruyelle@gmail.com>
2014-11-17 11:30:10 +0100

    weighted diected graph

---
+++ datastruct/weighteddigraph.go
@@ -0,0 +1,61 @@
+package datastruct
+
+// WeightedDigraph represents the graph data structure.
+type WeightedDigraph struct {
+	v     int
+	e     int
+	adj   [][]DirectedEdge
+	edges []DirectedEdge
+}
+
+type DirectedEdge struct {
+	v, w   int
+	weight int
+}
+
+// NewWeightedDigraph returns an adjacency-list graph representation.
+func NewWeightedDigraph(v int) *WeightedDigraph {
+	g := &WeightedDigraph{v: v}
+	g.adj = make([][]DirectedEdge, v)
+	return g
+}
+
+// DirectedEdge adds a new edge to the graph.
+func (g *WeightedDigraph) Edge(v, w, weight int) {
+	g.adj[v] = append(g.adj[v], DirectedEdge{v, w, weight})
+	g.e++
+	g.edges = append(g.edges, DirectedEdge{v, w, weight})
+}
+
+// DirectedEdges returns all the added edges.
+func (g *WeightedDigraph) Edges() []DirectedEdge {
+	return g.edges
+}
+
+// Adj returns the adjacent vertices of the vertex v.
+func (g *WeightedDigraph) Adj(v int) []DirectedEdge {
+	return g.adj[v]
+}
+
+// V returns the number of vertices.
+func (g *WeightedDigraph) V() int {
+	return g.v
+}
+
+// E returns the number of edges.
+func (g *WeightedDigraph) E() int {
+	return g.e
+}
+
+func (e DirectedEdge) From() int {
+	return e.v
+}
+
+func (e DirectedEdge) To() int {
+	return e.w
+}
+
+// Priority implements the Item interface from PriorityQueue.
+func (e DirectedEdge) Priority() int {
+	return e.weight
+}
