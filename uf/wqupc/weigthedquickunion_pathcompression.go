64c680f4bdc90706781f46666e2c16e4a12c035a
Thomas Bruyelle <thomas.bruyelle@gmail.com>
2015-06-24 16:44:29 +0200

    Weighted QuickUnion with 1-pass path compression

---
+++ uf/wqupc/weigthedquickunion_pathcompression.go
@@ -0,0 +1,44 @@
+package weightedquickunion_pathcompression
+
+type WeightedQuickUnion struct {
+	id []int
+	sz []int
+}
+
+func New(N int) WeightedQuickUnion {
+	qu := WeightedQuickUnion{}
+	qu.id = make([]int, N)
+	qu.sz = make([]int, N)
+	for i := 0; i < N; i++ {
+		qu.id[i] = i
+		qu.sz[i] = 1
+	}
+	return qu
+}
+
+func (qu WeightedQuickUnion) Connected(p, q int) bool {
+	return qu.root(p) == qu.root(q)
+}
+
+func (qu WeightedQuickUnion) Union(p, q int) {
+	i := qu.root(p)
+	j := qu.root(q)
+	if i == j {
+		return
+	}
+	if qu.sz[i] < qu.sz[j] {
+		qu.id[i] = j
+		qu.sz[j] += qu.sz[i]
+	} else {
+		qu.id[j] = i
+		qu.sz[i] += qu.sz[j]
+	}
+}
+
+func (qu WeightedQuickUnion) root(p int) int {
+	for p != qu.id[p] {
+		qu.id[p] = qu.id[qu.id[p]] // not tested
+		p = qu.id[p]
+	}
+	return p
+}
